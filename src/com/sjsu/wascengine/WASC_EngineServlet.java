487afd024a1af87d7f97b3d07a71a0a685f9c559
tstullich <timstullich@gmail.com>
2012-11-14 14:08:23 -0800

    Added some general documentation.

--- src/com/sjsu/wascengine/WASC_EngineServlet.java
+++ src/com/sjsu/wascengine/WASC_EngineServlet.java
@@ -3,13 +3,22 @@ package com.sjsu.wascengine;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+/**
+ * This is going to be the "main" class for the App Engine. It
+ * is going to listen on a certain port for an Http Request and
+ * should receive a pdf file. It is then going to be doing the
+ * work in order to figure out the appropriate rubric scores and
+ * return the final result as a JSON object.
+ * 
+ * @author Tim Stullich
+ *
+ */
 @SuppressWarnings("serial")
 public class WASC_EngineServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
+	   resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
-		resp.getWriter().println("Testing commit - keats");
-		
+		resp.getWriter().println("Testing commit - keats");		
 	}
 }

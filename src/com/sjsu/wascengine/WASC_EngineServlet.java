edaab82d67a31bc3e2b45c34b137de142329925c
tstullich <timstullich@gmail.com>
2012-11-14 17:00:41 -0800

    Added some more JSON related things. Can now successfully build a JSON object with all the necessary results.

--- src/com/sjsu/wascengine/WASC_EngineServlet.java
+++ src/com/sjsu/wascengine/WASC_EngineServlet.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import javax.servlet.http.*;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 /**
  * This is going to be the "main" class for the App Engine. It
@@ -23,9 +28,16 @@ public class WASC_EngineServlet extends HttpServlet {
 		resp.getWriter().println("Hello, world");
 		resp.getWriter().println("Testing commit - keats");
 		//Just a quick test to see how we can convert an object to JSON
-		Gson gson = new Gson();
 		String[] number = {"Tim", "Eddy", "Michael"};
-		String json = gson.toJson(number);
+		JsonObject obj = new JsonObject();
+		JsonArray numArray = new JsonArray();
+		for (int i = 0; i < number.length; i++)
+		{
+		   numArray.add(new JsonPrimitive(number[i]));
+		}
+		obj.addProperty("numOfContributors", number.length);
+		obj.add("contributors", numArray);
+		String json = obj.toString();
 		resp.getWriter().println(json);
 	}
 }
